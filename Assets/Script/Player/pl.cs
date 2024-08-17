fd3e3d3972b2e4189c6c86575881e1e0414441cf
seoil0404 <seoil6795@gmail.com>
2024-08-18 05:13:43 +0900

    hahaha

--- Assets/Script/Player/pl.cs
+++ Assets/Script/Player/pl.cs
@@ -41,29 +41,18 @@ public class pl : MonoBehaviour
 
         moveVec = new Vector3(x, 0 , z) * speed * Time.deltaTime;
         rigid.MovePosition(rigid.position + moveVec);
-
-        if(moveVec.sqrMagnitude == 0 ) {
-            return;
-        }
-        Vector3 inputDir = moveVec.normalized;
-        if (inputDir != Vector3.zero)//움직임을 멈췄을 때 다시 처음 각도로 돌아가는걸 막기위함
-        {
-            float rotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref rotationVelocity, smoothRotationTime);
-        }
+        Vector3 inputDir = moveVec;
         //각도를 구해주는 코드, 플레이어가 오른쪽 위 대각선으로 움직일시 그 방향을 바라보게 해준다
         //Mathf.Atan2는 라디안을 return하기에 다시 각도로 바꿔주는 Mathf.Rad2Deg를 곱해준다
         //Vector.up은 y axis를 의미한다
         //SmoothDampAngle을 이용해서 부드럽게 플레이어의 각도를 바꿔준다.
-
-        targetSpeed = moveSpeed * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, smoothMoveTime);
-        //현재스피드에서 타겟스피드까지 smoothMoveTime 동안 변한다
-        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
-
-        Quaternion dirQuat = Quaternion.LookRotation(moveVec);
-        Quaternion moveQuat = Quaternion.Slerp(rigid.rotation, dirQuat, 0.3f);
-        rigid.MoveRotation(moveQuat);
+        if(inputDir != Vector3.zero)
+        {
+            Quaternion dirQuat = Quaternion.LookRotation(moveVec);
+            Quaternion moveQuat = Quaternion.Slerp(rigid.rotation, dirQuat, 0.3f);
+            rigid.MoveRotation(moveQuat);
+        }
+        
     }
   
 }
