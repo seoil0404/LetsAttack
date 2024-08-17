85b56d005e806b58ca7ca93276fc391719e12de4
seoil0404 <seoil6795@gmail.com>
2024-08-17 20:48:30 +0900

    package

--- Assets/Script/Breakable/Breakable.cs
+++ Assets/Script/Breakable/Breakable.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
+        {
+            Destroy(gameObject);
+        }
     }
 }
