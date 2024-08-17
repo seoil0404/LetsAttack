cbe75b98787cc1b043cfb32f3aac7ea9cf781931
young060412 <103516158+young060412@users.noreply.github.com>
2024-08-18 04:18:25 +0900

    dw
    dw

--- Assets/Script/Player/pl.cs
+++ Assets/Script/Player/pl.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Jobs.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class pl : MonoBehaviour
 {
+
+    [SerializeField]//인스펙터에서만 참조 가능하게
+    private float smoothRotationTime;//target 각도로 회전하는데 걸리는 시간
+    [SerializeField]
+    private float smoothMoveTime;//target 속도로 바뀌는데 걸리는 시간
+    [SerializeField]
+    private float moveSpeed;//움직이는 속도
+    private float rotationVelocity;//The current velocity, this value is modified by the function every time you call it.
+    private float speedVelocity;//The current velocity, this value is modified by the function every time you call it.
+    private float currentSpeed;
+    private float targetSpeed;
+
     public VariableJoystick joy;
     public float speed;
 
@@ -12,8 +25,12 @@ public class pl : MonoBehaviour
     Animator anim;
     Vector3 moveVec;
 
+    public GameObject joystickPrefab;  // 조이스틱 프리팹을 참조할 변수
+   
+
     private void Awake()
     {
+        joy = Instantiate(joystickPrefab).GetComponent<VariableJoystick>();
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
     }
@@ -28,8 +45,23 @@ public class pl : MonoBehaviour
         if(moveVec.sqrMagnitude == 0 ) {
             return;
         }
+        Vector3 inputDir = moveVec.normalized;
+        if (inputDir != Vector3.zero)//움직임을 멈췄을 때 다시 처음 각도로 돌아가는걸 막기위함
+        {
+            float rotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref rotationVelocity, smoothRotationTime);
+        }
+        //각도를 구해주는 코드, 플레이어가 오른쪽 위 대각선으로 움직일시 그 방향을 바라보게 해준다
+        //Mathf.Atan2는 라디안을 return하기에 다시 각도로 바꿔주는 Mathf.Rad2Deg를 곱해준다
+        //Vector.up은 y axis를 의미한다
+        //SmoothDampAngle을 이용해서 부드럽게 플레이어의 각도를 바꿔준다.
 
-       Quaternion dirQuat = Quaternion.LookRotation(moveVec);
+        targetSpeed = moveSpeed * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, smoothMoveTime);
+        //현재스피드에서 타겟스피드까지 smoothMoveTime 동안 변한다
+        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+        Quaternion dirQuat = Quaternion.LookRotation(moveVec);
         Quaternion moveQuat = Quaternion.Slerp(rigid.rotation, dirQuat, 0.3f);
         rigid.MoveRotation(moveQuat);
     }
