5007c2b89902f6a9296bfecc3ecc10a16376ef76
seoil0404 <seoil6795@gmail.com>
2024-08-18 03:53:40 +0900

    Changee

--- Assets/Script/Breakable/Breakable.cs
+++ Assets/Script/Breakable/Breakable.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Breakable : MonoBehaviour
 {
     public GameObject explosion;
+    public float DestroyDelay;
+    private bool IsDestroy = false;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.layer == 6 || collision.gameObject.layer == 7)
         {
+            IsDestroy = true;
             Instantiate(explosion).GetComponent<Transform>().position = this.transform.position;
-            Destroy(gameObject);
+            Destroy(gameObject, DestroyDelay);
+        }
+    }
+
+    private void Update()
+    {
+        if(IsDestroy)
+        {
+            transform.position += Vector3.down*0.1f;
         }
     }
 }
